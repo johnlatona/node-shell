080a978887353d3f94ff17ecac767caceffb7683
Kevin Duarte <duartekevin91@gmail.com>
2018-07-31 12:28:11 -0400

    Upadte

--- bash.js
+++ bash.js
@@ -1,10 +1,13 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-
+  if( cmd  === 'ls'){
+    ls();
+  }
   if(cmd === 'pwd') {
     pwd(cmd);
   }
