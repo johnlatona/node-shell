7885dfabcf8ce8197c58753be2b842442c28c1de
johnlatona <johnbl24@aol.com>
2018-07-31 13:02:25 -0400

    cat

--- bash.js
+++ bash.js
@@ -1,14 +1,19 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
-
+const cat = require('./cat');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if( cmd  === 'ls'){
+  let split = cmd.split(' ');
+
+  if(split[0] === 'cat') {
+    cat(split[1]);
+  }
+  if(split[0]  === 'ls'){
     ls();
   }
-  if(cmd === 'pwd') {
+  if(split[0] === 'pwd') {
     pwd(cmd);
   }
   else {
